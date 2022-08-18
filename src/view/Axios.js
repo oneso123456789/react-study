941847127d3a2fdf801b8a11e31102fafb49625a
Crow <oneso123456789@gmail.com>
2022-08-18 19:11:11 +0900

    Axios사용해서 게시판 list 내용을 프론트에 표현하고싶음 -> 표현은 했는대 페이징 처리가 안됨

--- src/view/Axios.js
+++ src/view/Axios.js
@@ -3,18 +3,29 @@ import axios from "axios";
 
 
 function App() {
-    const [data, setdata] = useState('')
+    const [data, setdata] = useState([]);
     useEffect(() => {
         axios.get('/api/list')
             .then(response => setdata(response.data))
             .catch(error => console.log(error))
     }, []);
 
-
-
+    data.dtoList.map(item => (
+        item.bno
+    ))
     return (
+
         <div>
-            {data.totalPage}
+            {/*{data.dtoList}*/}
+                        <ul>
+                {data.dtoList.map(item => (
+                    <li key={item.bno}>
+                        <h3>{item.title}</h3>
+                        <p>{item.author}</p>
+                        <p>{item.content}</p>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
